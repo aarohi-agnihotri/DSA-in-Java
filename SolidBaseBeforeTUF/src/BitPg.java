6606a67632751f455822e431e9f2f7a07fbe0654
Aarohi Agnihotri <aarohiagnihotri.works@gmail.com>
2025-11-16 16:54:33 +0530

    resolved issue in func setBit + add setBit & updateBit func with notes

--- SolidBaseBeforeTUF/src/BitPg.java
+++ SolidBaseBeforeTUF/src/BitPg.java
@@ -14,30 +14,43 @@ class BitPg{
 
     static void setBit(){
         int n = 5;
-        int pos = 2;
+        int pos = 1;
         int bitMask = 1 << pos;
-
-        if((bitMask & n) == 0)
-        System.out.println("bit was 0");
-        else
-        System.out.println("bit was 1");
+        int newNum = bitMask | n;
+        System.out.println(newNum);
         return;
     }
 
     static void clearBit(){
-        
+        int n = 5;
+        int pos = 2;
+        int bitMask = 1 << pos;
+        int notOfBitMask = ~(bitMask);
+        int newNum = notOfBitMask & n;
+        System.out.println(newNum);
     }
+
     static void updateBit(){
-        
+        Scanner sc= new Scanner(System.in);
+        System.out.println("If want to update bit to 1, then enter 1 , else 0");
+        int oper = sc.nextInt();
+        int n = 5, newNum;
+        int pos= 1;
+        int bitMask = 1 << pos;
+        if(oper == 1){
+             newNum = n | bitMask;
+        }
+        else{
+            int notOfBitMask = ~(bitMask);
+             newNum = notOfBitMask & n;
+        }
+        System.out.println(newNum);
     }
     
     public static void main(String args[]){
-        Scanner sc= new Scanner(System.in);
-        // System.out.println("Enter number of rows:");
-        // int r=sc.nextInt();
         // getBit();
-        setBit();
+        // setBit();
         // clearBit();
-        // updateBit();
+        updateBit();
         }
     }
